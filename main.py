f0a3cfb51634aaa3a5379132f7989f730ec4d221
petitmi <petitmi001@gmail.com>
2022-12-13 14:50:02 -0800

    revise chords

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from music_generator import *
 from music21 import *
+import os
 def main():
     tmp = MusicGenerator(note_object=[['C','D','G'],'1/1'],num_bars = 4)
     _mld = tmp.mix_melody_chords()
-    _mld.show()
+    # _mld.show()
+    fp_output = ['file.mid','score-1.png','score.musicxml']
+    print(os.getcwd())
+    for fp in fp_output:
+        if os.path.exists(fp):
+            os.remove(fp)
     _mld.write('midi', fp='file.mid')
     _mld.write('musicxml.png',fp = 'score.png')
-    print(_mld)
+    print("{0} {1} {2} finished writing ".format('\033[1m',str(fp),'\033[0m'))
 
 if __name__ == "__main__":
     main()
--- music_generator.py
+++ music_generator.py
@@ -1,68 +1,79 @@
 from music_analyzor import *
 from music21 import *
+from music21 import chord
+
+# color refer: https://xdevs.com/guide/color_serial/
 
 class MusicGenerator(MusicAnalyzor):
     def __init__(self, note_object,num_bars):
         MusicAnalyzor.__init__(self, note_object, num_bars)
         self.Key = self.key_setting()
+        print("{0}rhythm beats{1} \nstart...".format('\033[1m','\033[0m'))
         self.Beats = self.rhythm_setting()
+        print("finished...\n {2}{3}{4}".format('\033[1m','\033[0m','\033[94m',str(self.Beats),'\033[0m'))
+        print("{0}chords progression{1} \nstart...".format('\033[1m','\033[0m'))
         self.ChordsPrg = self.chords_setting()
-        print(self.Beats, self.ChordsPrg)
+        print("finished...\n {2}{3}{4}".format('\033[1m','\033[0m','\033[92m',str(self.ChordsPrg),'\033[0m'))
 
     def chords_generate(self):
         chords_lst=[]
         for j in range(4): #four 
-            for i in range(1):
-                chord = roman.RomanNumeral(self.ChordsPrg[j])
-                chord.key = self.Key
-                chord.duration.type = 'whole'
-                chords_lst.append(chord)
+            for i in range(2):
+                _chord = roman.RomanNumeral(self.ChordsPrg[j])
+                _chord= " ".join([str(p) for p in _chord.pitches])
+                # change from RomanNumeral to origin chords
+                crd = chord.Chord(_chord)
+                crd.key = self.Key
+                crd.duration.type = 'half'
+                chords_lst.append(crd)
         return chords_lst
 
     def melody_generate(self):
         '''generate melody_notes list based on key, chord progression and beats.'''
+        print("{0}melody{1} \nstart...".format('\033[1m','\033[0m'))
         cnt_notes_bar = [len(x) for x in self.Beats]
         _melody_notes = []
         for num_beat in range(len(self.Beats)):
             num_chord = num_beat//4
             rf = roman.RomanNumeral(self.ChordsPrg[num_chord], self.Key) # transfer roman numerals to chords based on key.
             chordNotes = rf.normalOrder # get the pitches of chords
-            # randomly generate notes based on chords.
-            _melody_notes.append(random.choices(chordNotes,k=cnt_notes_bar[num_beat]))
-        print('_melody_notes',_melody_notes)
+            _melody_notes.append(random.choices(chordNotes,k=cnt_notes_bar[num_beat])) # randomly generate notes based on chords.
+
         melody_lst = []
+        # set beats
         for num_melody in range(len(_melody_notes)):  
             itm= _melody_notes[num_melody]
+            # type of the note beat
+            if len(itm) == 4: _type = '16th' # Sixteenth note type
+            elif len(itm) == 2: _type = 'eighth' # Eighth note type
+            elif len(itm) == 1: _type = 'quarter' # Quarter note type
             for num_itm in range(len(itm)):
                 itm_melody = itm[num_itm]
-                if len(itm) == 4: _type = '16th'
-                elif len(itm) == 2: _type = 'eighth'
-                elif len(itm) == 1: _type = 'quarter'
+                # combine to note.Note
                 if self.Beats[num_melody][num_itm] == '0':
-                    _note = note.Rest(type = _type)
+                    _note = note.Rest(type = _type) # Rest note
                 else:
-                    _note = note.Note(self.note_num[str(itm_melody)][0], type = _type)
+                    _note = note.Note(self.note_num[str(itm_melody)][0], type = _type) # Pitch note
                 melody_lst.append(_note)
+        print("finished...\n{2} {3}{4} ".format('\033[1m','\033[0m','\033[93m',str(melody_lst),'\033[0m'))
         return melody_lst
 
-        # pc.show('midi')
     def mix_melody_chords(self):
         s = stream.Score()
         mm1 = tempo.MetronomeMark(number=88)
-        # s.append(mm1)
-        p1 = stream.Part()
-        p1.append(mm1)
+        # create melody part of the score
+        p_melody = stream.Part()
+        p_melody.append(mm1)
         chords_lst = self.chords_generate() # generate chords
         melody_lst = self.melody_generate() # generate melody
         for note in melody_lst:
-            p1.append(note)
-        p2 = stream.Part()
-        p2.append(mm1)
-
+            p_melody.append(note)
+        # create chords part of the score
+        p_chords = stream.Part()
+        p_chords.append(mm1)
         for num_chord in range(len(chords_lst)):
-            # print(num_chord*2, chords_lst[num_chord])
-            p2.insert(num_chord*4, chords_lst[num_chord])
-        p2.show()
-        s.insert(0,p1)
-        s.insert(0,p2)
+            p_chords.insert(num_chord*2, chords_lst[num_chord])
+        # mix the melody and chords
+        s.insert(0,p_melody)
+        s.insert(0,p_chords)
         return s
